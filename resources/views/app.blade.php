a6a79c75e7183dbbc9fdcf30275f0ffbff7d1009
shortnd <ococncol@gmail.com>
2018-06-26 23:34:14 -0400

    added two views one or the app and one for login

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content={{ csrf_token() }}>
+
+    <link href="/css/app.css" rel="stylesheet" type="text/css"/>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+
+    <title>Roast</title>
+    <script type='text/javascript'>
+      window.Laravel = <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+    ]); ?>
+    </script>
+
+  </head>
+  <body>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+
+    <script src="js/app.js" type="text/javascript"></script>
+  </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, inital-scle=1.0">
+    <title>Roast</title>
+</head>
+<body>
+    <a href="/login/google">
+        Login with Google
+    </a>
+</body>
+</html>
