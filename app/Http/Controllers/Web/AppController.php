806dbe9eee256760d44b7d6d31383ef40d891507
shortnd <ococncol@gmail.com>
2018-06-26 23:38:30 -0400

    started writting web controller and route

---
+++ app/Http/Controllers/Web/AppController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+class AppController extends Controller
+{
+    public function getApp()
+    {
+        return view('app');
+    }
+}
