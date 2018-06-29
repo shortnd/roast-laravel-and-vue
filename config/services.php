f54776c27da530f119dce7ab49c5b046f3eae2aa
shortnd <ococncol@gmail.com>
2018-06-29 19:38:28 -0400

    added facebook auth

--- config/services.php
+++ config/services.php
@@ -14,6 +14,12 @@ return [
     |
     */
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT')
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
